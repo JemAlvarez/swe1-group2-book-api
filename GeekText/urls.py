ebc4de2ccf54384ff3fc99f5d2bf7821f81a2a8a
Jem <jemalvarez22@gmail.com>
2022-09-26 18:00:27 -0400

    Actually finish feature 4, authentication for book creation
    including daniels PR here

--- GeekText/urls.py
+++ GeekText/urls.py
@@ -4,8 +4,6 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'books', views.BookViewSet)
-# router.register(r'author', views.AuthorViewSet)
 router.register(r'publisher', views.PublisherViewSet)
 router.register(r'genre', views.GenreViewSet)
 
@@ -13,8 +11,9 @@ router.register(r'genre', views.GenreViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('books/', views.book),
     path('books/<str:isbn>', views.getBookByISBN),
-    path('author', views.author),
+    path('author/', views.author),
     path('author/<str:name>/books', views.getAuthorBooks),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
