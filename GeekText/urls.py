f619557a407eca5447ac02ddb75152c82afe5f47
Santiago Arboleda <105461397+SRueda1@users.noreply.github.com>
2022-11-26 19:00:37 -0400

    Add Rating in urls

--- GeekText/urls.py
+++ GeekText/urls.py
@@ -2,10 +2,12 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
+from .views import RatingViewSets
 
 router = routers.DefaultRouter()
 router.register(r'publisher', views.PublisherViewSet)
 router.register(r'genre', views.GenreViewSet)
+router.register('rating', RatingViewSets)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -17,5 +19,6 @@ urlpatterns = [
     path('author/<str:name>/books', views.getAuthorBooks),
     path('api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
-    path('wishlist/<str:user>', views.getWishlistByUser)
+    path('wishlist/<str:user>', views.getWishlistByUser),
+    path('api/', include(router.urls)), 
 ]
